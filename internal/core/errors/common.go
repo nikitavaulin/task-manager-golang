4243759905a1dc32e149fde2d08313f0cea7a834
nsvaulin <mr.vau2017@mail.ru>
2026-05-06 14:05:53 +0500

    refactor: add common file to core for domain, add ErrNoRule to core_errors

--- internal/core/errors/common.go
+++ internal/core/errors/common.go
@@ -6,4 +6,5 @@ var (
 	ErrNotFound        = errors.New("not found")
 	ErrInvalidArgument = errors.New("invalid argument")
 	ErrConflict        = errors.New("conflict")
+	ErrNoRepeatRule    = errors.New("repeat rule is empty")
 )
