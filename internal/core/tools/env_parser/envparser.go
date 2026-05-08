e3e21a1cc4e84a02708a79cd1865c75b65b6b92f
nsvaulin <mr.vau2017@mail.ru>
2026-05-08 11:34:33 +0500

    feat: impl tools password, hasher, jwt

--- internal/core/tools/env_parser/envparser.go
+++ internal/core/tools/env_parser/envparser.go
@@ -1,11 +1,22 @@
 package tools_envparser
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func GetEnvVarOrDefault(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
+	value, err := GetEnvVar(key)
+	if err != nil {
 		return defaultValue
 	}
 	return value
+}
+
+func GetEnvVar(key string) (string, error) {
+	value := os.Getenv(key)
+	if value == "" {
+		return "", fmt.Errorf("failed to get env var with key=%s", key)
+	}
+	return value, nil
 }
